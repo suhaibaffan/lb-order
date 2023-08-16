5cf4c104c1f68d0b0c0cec28019c22c4483b5b0e
Suhaib Affan <suhaibaffan91@gmail.com>
2023-08-16 19:21:00 -0400

    Initial setup.

---
+++ src/orders/orders.controller.ts
@@ -0,0 +1,17 @@
+import { Controller, Get } from '@nestjs/common';
+import { OrdersService } from './orders.service';
+
+@Controller('orders')
+export class OrdersController {
+  constructor(private readonly ordersService: OrdersService) {}
+
+  @Get()
+  getOrders(): [] {
+    return this.ordersService.getOrders();
+  }
+
+  @Get('healthz')
+  isUp(): string {
+    return 'Service is up!';
+  }
+}
